138f268120236de018a063f9866a87660115bc74
Stiven Grisales <carlosstivengrisales@gmail.com>
2023-07-13 18:46:25 -0500

    Se crea el formulario poscompra con sus variables

--- lib/screens/CartScreen_.dart
+++ lib/screens/CartScreen_.dart
@@ -16,141 +16,149 @@ class Screencart extends StatefulWidget {
 class _ScreencartState extends State<Screencart> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<Carrito>(builder: (context, carrito, child) {
-      return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 3, 102, 119),
-          title: Text("Pedidos"),
-          elevation: 0,
-        ),
-        body: Container(
-            child: carrito.items.length == 0
-                ? Center(
-                    child: Text("No tiene Pedidos"),
-                  )
-                : ListView(children: [
-                    Column(
-                      children: <Widget>[
-                        for (var item in carrito.items.values)
-                          Card(
-                            margin: EdgeInsets.all(10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: <Widget>[
-                                Image.asset(
-                                  "assets/" + item.imagen,
-                                  width: 100,
-                                ),
-                                Expanded(
-                                  child: Container(
-                                    padding: EdgeInsets.all(5),
-                                    height: 100,
-                                    child: Column(
-                                      children: <Widget>[
-                                        Text(item.nombre),
-                                        Text(item.precio.toString() +
-                                            " * " +
-                                            item.unidad),
-                                        Row(
-                                          mainAxisSize: MainAxisSize.min,
-                                          children: <Widget>[
-                                            Container(
-                                                child: IconButton(
-                                              icon: Icon(Icons.remove),
-                                              onPressed: () {
-                                                setState(() {
-                                                  carrito
-                                                      .decrementarCantidadItem(
-                                                          item.id);
-                                                });
-                                              },
-                                            )),
-                                            Container(
-                                              child: Center(
-                                                  child: Text(item.cantidad
-                                                      .toString())),
-                                            ),
-                                            Container(
-                                                child: IconButton(
-                                              icon: Icon(Icons.add),
-                                              onPressed: () {
-                                                setState(() {
-                                                  carrito
-                                                      .incrementarCantidadItem(
-                                                          item.id);
-                                                });
-                                              },
-                                            )),
-                                          ],
-                                        )
-                                      ],
+    return Consumer<Carrito>(
+      builder: (context, carrito, child) {
+        return Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromARGB(255, 3, 102, 119),
+              title: Text("Pedidos"),
+              elevation: 0,
+            ),
+            body: Container(
+                child: carrito.items.length == 0
+                    ? Center(
+                        child: Text("No tiene Pedidos"),
+                      )
+                    : ListView(children: [
+                        Column(children: <Widget>[
+                          for (var item in carrito.items.values)
+                            Card(
+                              margin: EdgeInsets.all(10),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Image.asset(
+                                    "assets/" + item.imagen,
+                                    width: 100,
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      padding: EdgeInsets.all(5),
+                                      height: 100,
+                                      child: Column(
+                                        children: <Widget>[
+                                          Text(item.nombre),
+                                          Text(item.precio.toString() +
+                                              " * " +
+                                              item.unidad),
+                                          Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              Container(
+                                                  child: IconButton(
+                                                icon: Icon(Icons.remove),
+                                                onPressed: () {
+                                                  setState(() {
+                                                    carrito
+                                                        .decrementarCantidadItem(
+                                                            item.id);
+                                                  });
+                                                },
+                                              )),
+                                              Container(
+                                                child: Center(
+                                                    child: Text(item.cantidad
+                                                        .toString())),
+                                              ),
+                                              Container(
+                                                  child: IconButton(
+                                                icon: Icon(Icons.add),
+                                                onPressed: () {
+                                                  setState(() {
+                                                    carrito
+                                                        .incrementarCantidadItem(
+                                                            item.id);
+                                                  });
+                                                },
+                                              )),
+                                            ],
+                                          )
+                                        ],
+                                      ),
                                     ),
                                   ),
-                                ),
-                                Container(
-                                  height: 100,
-                                  width: 90,
-                                  margin: EdgeInsets.only(right: (8)),
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(10),
-                                    color: Color.fromARGB(69, 3, 102, 119),
-                                  ),
-                                  child: Center(
-                                    child: Text("S/" +
-                                        (item.precio * item.cantidad)
-                                            .toString()),
-                                  ),
-                                )
-                              ],
+                                  Container(
+                                    height: 100,
+                                    width: 90,
+                                    margin: EdgeInsets.only(right: (8)),
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Color.fromARGB(69, 3, 102, 119),
+                                    ),
+                                    child: Center(
+                                      child: Text("S/" +
+                                          (item.precio * item.cantidad)
+                                              .toString()),
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Row(children: <Widget>[
+                              Expanded(
+                                child: Text("Subtotal: "),
+                              ),
+                              Text("s/." + carrito.subTotal.toStringAsFixed(3)),
+                            ]),
                           ),
-                        Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Row(children: <Widget>[
-                            Expanded(
-                              child: Text("Subtotal: "),
-                            ),
-                            Text("s/." + carrito.subTotal.toStringAsFixed(3)),
-                          ]),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Row(children: <Widget>[
-                            Expanded(
-                              child: Text("Impuesto"),
-                            ),
-                            Text("S/." + carrito.impuesto.toStringAsFixed(3)),
-                          ]),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(15),
-                          child: Row(children: <Widget>[
-                            Expanded(
-                              child: Text("Total"),
-                            ),
-                            Text("S/." + carrito.total.toStringAsFixed(3)),
-                          ]),
-                        ),
-                      ],
-                    )
-                  ])),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-           var alert = SnackBar(
-                            content: Text('No puedes hacer Checkout, por favor llene el carrito'));
-                        carrito.numeroItems != 0
-                            ? Navigator.of(context).push(MaterialPageRoute(
-                                builder: (BuildContext) => FormCompra()))
-                            : ScaffoldMessenger.of(context)
-                                .showSnackBar(alert);
-          },
-          backgroundColor: Color.fromARGB(255, 3, 102, 119),
-          child: Icon(
-            Icons.send,
-            color: Colors.amber,
-          ),
-        ),
-      );
-    });
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Row(children: <Widget>[
+                              Expanded(
+                                child: Text("Impuesto"),
+                              ),
+                              Text("S/." + carrito.impuesto.toStringAsFixed(3)),
+                            ]),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(15),
+                            child: Row(children: <Widget>[
+                              Expanded(
+                                child: Text("Total"),
+                              ),
+                              Text("S/." + carrito.total.toStringAsFixed(3)),
+                            ]),
+                          ),
+                          const SizedBox(height: 30),
+                          Container(
+                              margin: const EdgeInsets.only(top: 43.0),
+                              width: MediaQuery.of(context).size.width * 0.62,
+                              child: ElevatedButton(
+                                  child: const Text(
+                                    "Realizar Compra",
+                                    style: TextStyle(
+                                        color: Color.fromARGB(255, 3, 102, 119),
+                                        fontFamily: 'RadikalMedium',
+                                        fontSize: 14),
+                                  ),
+                                  onPressed: () {
+                                    var alert = SnackBar(
+                                        content: Text(
+                                            'No puedes hacer Checkout, por favor llene el carrito'));
+                                    carrito.numeroItems != 0
+                                        ? Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (BuildContext) =>
+                                                    FormCompra()))
+                                        : ScaffoldMessenger.of(context)
+                                            .showSnackBar(alert);
+                                  }))
+                        ])
+                      ])));
+      },
+    );
   }
 }
--- lib/screens/posCompra.dart
+++ lib/screens/posCompra.dart
@@ -2,31 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:integracion_app/screens/shoppingscreen.dart';
+import 'package:provider/provider.dart';
+
+import '../carrito/carrito.dart';
 
 class PosCompra extends StatelessWidget {
   final String _nombreUs;
   final String _apellidoUs;
   final String _direccionUs;
   final String _postalUs;
-  const PosCompra(this._nombreUs, this._apellidoUs, this._direccionUs,
-      this._postalUs,
+  const PosCompra(
+      this._nombreUs, this._apellidoUs, this._direccionUs, this._postalUs,
       {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 3, 102, 119),
-        scrolledUnderElevation: 50,
-        title: const Text("Compra OK"),
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(children: [
-          Text("GRACIAS POR TU COMPRA"),
-          Text(_nombreUs)
-        ]
-      ),),
-    );
+    return Consumer<Carrito>(builder: (context, carrito, child) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 3, 102, 119),
+          scrolledUnderElevation: 50,
+          title: const Text("Compra OK"),
+          elevation: 0,
+        ),
+        body: Center(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "GRACIAS POR TU COMPRA",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  "Nombre: " +
+                      _nombreUs +
+                      " " +
+                      _apellidoUs +
+                      "\nEnvio: " +
+                      _direccionUs +
+                      "\nPostal: " +
+                      _postalUs +
+                       "\nTotal: S/."+
+                      carrito.total.toStringAsFixed(3),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ]),
+        ),
+      );
+    }
+   );
   }
 }
